0748fd712d4a0ab5c0f0b9805dfeefaa8f8a8a2d
mattiadebonis <mattia.debonis@gmail.com>
2021-07-18 21:05:18 +0200

    Correzioni seed

--- config/pacchetti.php
+++ config/pacchetti.php
@@ -3,12 +3,12 @@ return [
     [
         "name" => "Viaggio in Kenya",
         "destination" => "Kenya",
-        "price" => 2354,00
+        "price" => 2354
     ],
     [
         "name" => "Viaggio a New York",
         "destination" => "New York",
-        "price" => 5002,00
+        "price" => 5002
     ],
 
 ]
